a2cb7486f1020f3785cd5817e24368c69bfcdeb5
entl <maxvorobyov01@gmail.com>
2025-08-03 21:05:35 +0100

    feat: add validator

---
+++ internal/utils/validator.go
@@ -0,0 +1,15 @@
+package utils
+
+import "github.com/go-playground/validator/v10"
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+func NewCustomValidator(validator *validator.Validate) *CustomValidator {
+	return &CustomValidator{validator: validator}
+}
